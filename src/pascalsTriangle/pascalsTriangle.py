fab96d93e41681fc834f099e19e46c9fca65f1da
quezixiong <575303442@qq.com>
2014-12-06 18:19:28 +0800

    add pascalsTriangle

---
+++ src/pascalsTriangle/pascalsTriangle.py
@@ -0,0 +1,27 @@
+# Given numRows, generate the first numRows of Pascal's triangle.
+#
+# For example, given numRows = 5,
+# Return
+#
+# [
+#      [1],
+#     [1,1],
+#    [1,2,1],
+#   [1,3,3,1],
+#  [1,4,6,4,1]
+# ]
+
+
+class Solution:
+    # @return a list of lists of integers
+    def generate(self, numRows):
+        if numRows == 0:
+            return []
+        ret = [[1]]
+        for n in range(1, numRows):
+            tmp_ret = [1]
+            for i in range(1, n):
+                tmp_ret.append(ret[n-1][i-1] + ret[n-1][i])
+            tmp_ret.append(1)
+            ret.append(tmp_ret)
+        return ret
