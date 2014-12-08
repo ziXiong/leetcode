768fc4e78923bc5aa80f4c3b649c4a485d13c247
quezixiong <575303442@qq.com>
2014-12-08 15:48:15 +0800

    in sameTree, better way to write judgement statement

--- src/sameTree/sameTree.py
+++ src/sameTree/sameTree.py
@@ -1,11 +1,17 @@
+# Given two binary trees, write a function to check if they are equal or not.
+# Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+#
 # Definition for a  binary tree node
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 
+# Knowledge Point:
+# * regular recursion way to solve tree-related problems
+# * better way to write judgement statement
 class Solution:
     # @param p, a tree node
     # @param q, a tree node
@@ -13,39 +19,8 @@ class Solution:
     def isSameTree(self, p, q):
         if p is None and q is None:
             return True
-        elif p is None or q is None:
-            return False
-        elif p.val != q.val:
-            return False
-        elif self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right):
-            return True
+        elif p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         else:
             return False
 
-
-def main():
-    treelist1 = []
-    treeroot1 = TreeNode(0)
-    treelist1.append(treeroot1)
-    for i in range(1, 10):
-        tree = TreeNode(i)
-        treelist1.append(tree)
-        if i % 2 == 0:
-            treelist1[int(i/2)-1].right = tree
-        else:
-            treelist1[int(i/2)].left = tree
-    treelist2 = []
-    treeroot2 = TreeNode(0)
-    treelist2.append(treeroot2)
-    for i in range(1, 10):
-        tree = TreeNode(i)
-        treelist2.append(tree)
-        if i % 2 == 0:
-            treelist2[int(i/2)-1].right = tree
-        else:
-            treelist2[int(i/2)].left = tree
-    solution = Solution()
-    print(solution.isSameTree(treeroot1, treeroot2))
-
-if __name__ == '__main__':
-    main()
