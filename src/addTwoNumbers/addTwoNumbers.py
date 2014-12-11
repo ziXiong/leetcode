e9d8600c775a60aa294419ed63cd5ecf5c4ac871
quezixiong <575303442@qq.com>
2014-12-11 15:45:25 +0800

    fix addTwoNumbers to be accepted

--- src/addTwoNumbers/addTwoNumbers.py
+++ src/addTwoNumbers/addTwoNumbers.py
@@ -14,34 +14,29 @@
 class Solution:
     # @return a ListNode
     def addTwoNumbers(self, l1, l2):
-        if l1 is None:
-            return l2
-        if l2 is None:
-            return l1
-        flag = 0
+        if l1 is None or l2 is None:
+            return l1 if l2 is None else l2
         cursor1 = l1
         cursor2 = l2
-        while True:
-            cursor1 = cursor1.next
-            if cursor1 is None:
-                flag += 1
-                cursor1 = l2
-                if flag == 2:
-                    cursor2 = cursor2.next
-                    while cursor1 is not None:
-                        cursor2.val += cursor1.val
-                        cursor1 = cursor1.next
-                        cursor2 = cursor2.next
+        she_is_cute = 0
+        while cursor1.next is not None:
+            cursor1.val += cursor2.val + she_is_cute
+            if cursor1.val >= 10:
+                cursor1.val %= cursor1.val
+                she_is_cute = 1
+            if cursor1.next:
+                cursor1 = cursor1.next
+            else:
+                cursor1.next = cursor2.next
+                if cursor1.next is None:
+                    if she_is_cute:
+                        cursor1.next = ListNode(she_is_cute)
                     return l1
-            cursor2 = cursor2.next
-            if cursor2 is None:
-                flag += 1
-                cursor2 = l1
-                if flag == 2:
-                    while cursor1 is not None:
-                        cursor1.val += cursor2.val
-                        cursor1 = cursor1.next
-                        cursor2 = cursor2.next
-                    return l2
+                cursor1 = cursor1.next
+                cursor2.next = None
+            if cursor2.next:
+                cursor2 = cursor2.next
+            else:
+                cursor2.val = 0
 
-        
+
